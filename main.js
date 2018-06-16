bea4147eff139be8c71f61c3bc0ec448051dc6a1
Michael <learicist@gmail.com>
2018-06-16 03:04:20 -0400

    damn git..

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+<<<<<<< HEAD
+$(document).ready(function() {
+	$(".dropdown").mouseenter(function() {
+		$(this).css("background-color", "white");
+	});
+
+	$(".dropdown").mouseleave(function() {
+		$(this).css("background-color", "black");
+	});
+
+	$("#myName").mouseenter(function() {
+		$(this).css("background-color", "white");
+		$(this).css("color", "black");
+	});
+
+	$("#myName").mouseleave(function() {
+		$(this).css("background-color", "#3374ad");
+		$(this).css("color", "white");
+	});
+
+	$(window).scroll(function() {
+		// If page is scrolled more than 50px
+		if ($(this).scrollTop() >= 50) {   
+			// Fade in the arrow
+			$('#scrollUp').fadeIn(200);    
+		} else {
+			$('#scrollUp').fadeOut(200);  
+		}
+	}); 
+
+	// When arrow is clicked
+	$('#scrollUp').click(function() {      
+		$('body, html').animate({
+			 // Scroll to top of body
+			scrollTop : 0                      
+		}, 500);
+	}); 
+
+
+});
+
+=======
 $(document).ready(function() {
   $(".dropdown").mouseenter(function() {
     $(this).css("background-color", "white");
@@ -34,3 +76,4 @@ $(document).ready(function() {
   
 });
 
+>>>>>>> 19255f05ce4d97116226af6a464e3ed5e8763459
