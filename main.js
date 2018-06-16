718c116114f3d383edb79eda996b2d0dbf80fb86
Michael <learicist@gmail.com>
2018-06-16 15:24:43 -0400

    wtf m8

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
 	$(".dropdown").mouseenter(function() {
 		$(this).css("background-color", "white");
@@ -38,42 +37,3 @@ $(document).ready(function() {
 
 
 });
-
-=======
-$(document).ready(function() {
-  $(".dropdown").mouseenter(function() {
-    $(this).css("background-color", "white");
-  });
-  
-  $(".dropdown").mouseleave(function() {
-    $(this).css("background-color", "black");
-  });
-  
-  $("#myName").mouseenter(function() {
-    $(this).css("background-color", "white");
-    $(this).css("color", "black");
-  });
-  
-  $("#myName").mouseleave(function() {
-    $(this).css("background-color", "#3374ad");
-    $(this).css("color", "white");
-  });
-  
-  $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#scrollUp').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#scrollUp').fadeOut(200);   // Else fade out the arrow
-    }
-  }); 
-  
-  $('#scrollUp').click(function() {      // When arrow is clicked
-    $('body, html').animate({
-        scrollTop : 0                       // Scroll to top of body
-      }, 500);
-    }); 
-  
-  
-});
-
->>>>>>> 19255f05ce4d97116226af6a464e3ed5e8763459
